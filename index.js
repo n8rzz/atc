35b39e5ac345af654289bb4bad02ee6721c0f0e0
Nate Geslin <teamtomkins23@gmail.com>
2016-12-17 22:17:12 -0600

    feature/ATC-219 - Adds socket.io to server and client index.js files * Adds chalk to server/index.js * Sets up initial socket POC for handshake, connect, and disconnect events

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const express = require('express');
+const http = require('http');
+const SocketIO = require('socket.io');
+const path = require('path');
+const chalk = require('chalk');
+const uuid = require('uuid4');
+const _has = require('lodash/has');
+const _omit = require('lodash/omit');
 
 const app = express();
-const http = require('http').Server(app);
-const path = require('path');
+const server = http.Server(app);
+const io = new SocketIO(server);
 
-const PORT = 3003;
+const PORT = process.env.PORT || 3003;
+let sockets = {};
 
 app.use('/assets', express.static(path.join(__dirname, '/assets')));
 
@@ -12,6 +20,43 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/index.html'));
 });
 
-http.listen((process.env.PORT || PORT), () => {
-    console.log(`Listening on PORT ${PORT}`);
+io.on('connection', (socket) => {
+    const clientId = socket.handshake.query.clientId;
+
+    if (!uuid.valid(clientId)) {
+        console.log('ERROR');
+    }
+
+    const currentUser = {
+        id: socket.id
+    };
+
+    if (!_has(sockets, clientId)) {
+        sockets[clientId] = currentUser;
+    }
+
+    console.log(chalk.magenta(`[INFO] User ${clientId} connected!`));
+    console.log(chalk.dim(`${Object.keys(sockets).length} total users connected`));
+
+    io.emit('userJoin');
+
+    socket.on('ding', () => {
+        console.log(chalk.dim('::: DING'));
+
+        socket.emit('dong');
+    });
+
+    socket.on('disconnect', () => {
+        if (_has(sockets, clientId)) {
+            console.log(chalk.red('[INFO] User disconnected!'));
+            sockets = _omit(sockets, clientId);
+        }
+
+        console.log(chalk.dim(`${Object.keys(sockets).length} total users connected`));
+        socket.broadcast.emit('userDisconnect', { clientId: clientId });
+    });
+});
+
+server.listen(PORT, () => {
+    console.log(chalk.green.bold(`Listening on PORT ${PORT}`));
 });
--- src/assets/scripts/index.js
+++ src/assets/scripts/index.js
@@ -3,6 +3,22 @@ require('raf').polyfill();
 import 'babel-polyfill';
 import $ from 'jquery';
 import App from './App';
+import io from 'socket.io-client';
+import uuid from 'uuid4';
+
+const clientId = uuid();
+const socket = io({ query: `clientId=${clientId}` });
+
+const initiateSocketWithDingDong = () => {
+    const startPingTime = Date.now();
+
+    socket.emit('ding');
+    socket.on('dong', () => {
+        const latency = Date.now() - startPingTime;
+
+        console.log(`::: Ping: ${latency}ms`);
+    });
+};
 
 /**
  * Entry point for the application.
@@ -10,6 +26,8 @@ import App from './App';
  * Provides a way to grab the `body` element of the document and pass it to the app.
  */
 export default (() => {
+    initiateSocketWithDingDong();
+
     const airportLoadList = window.AIRPORT_LOAD_LIST;
     const $body = $('body');
     const app = new App($body, airportLoadList);
